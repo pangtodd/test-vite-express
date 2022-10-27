6e3178324a0d3fcf0dd90504eb654aaf6b04011e
Todd Pangilinan <pang.todd@gmail.com>
2022-10-26 23:23:08 -0700

    added api directory for api.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>HELLO + WORLD</h1>
+      <h1>HELLO + WOLDD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
