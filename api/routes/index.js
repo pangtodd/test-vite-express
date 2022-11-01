c2a8fd2c44fa10ec903c272ce2a9b04b83c59a4d
Todd Pangilinan <pang.todd@gmail.com>
2022-11-01 09:22:19 -0700

    added stock files from npx prisma install

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Xpress' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+// const express = require('express')
+// const app = express()
+// var router = express.Router();
+
+// app.get('/', (req, res) => {
+//   res.send('You got served.')
+// })
